903b9c388687aeb0cbaa81d6fa784502f23e5a77
Jeronimo Rodriguez-Escobar <38987055+Cumaribo@users.noreply.github.com>
2022-04-08 01:07:22 -0400

    Update DEFORESTACION.R
    First test. Map yearly forest loss

--- DEFORESTACION.R
+++ DEFORESTACION.R
@@ -10,49 +10,29 @@ library(sp)
 
 #This needs to be converted into using gdal or terra. Not raster anymore 
 
-
-
 # Setting working directory and temporary folder -----------------------------------------
-path=("Y:/ViktoriaShare/BioMapCol/Deforestation_rasters")
-setwd(path)
-dir.create('tempfiledir')
-tempdir=paste(getwd(),'tempfiledir', sep="/")
-rasterOptions(tmpdir=tempdir)
+
 
 #1. Cargar los raster de Bosque Armonizados de Hansen ------------
 #Estos son los raster procesados por Jeronimo usando Hansen
-H2018 <- raster("Y:/Shared_summer_21/Maps_hansen/Armomized_nomsk/armonized_2018a.tif")
-H2017<- raster("Y:/Shared_summer_21/Maps_hansen/Armomized_nomsk/armonized_2017a.tif")
+# Set list of rasters to process
+tiffs <- list.files('.'pattern='tif')
+#load rasters
+tiffs1<-list()
+for(i in (1:length(tiffs)){
+  tiffs1[i]<-tiffs[i]
+  }
 
-#2. Reclassify each Hansen raster to convert NA values into Zeros ------
-# bc the values of the raster are only 1 and NA, and to calculate forest lost 
-# we need binary rasters of 0 and 1. 
-#*** IMPORTANTE REVISAR SI PARA CALCULAR
-# PERDIDA (RESTAR) ES NECESARIO RECLASIFICAR NA POR CERO. PORQUE PARA HACER MERGE
-# LOS RASTER NO DEBEN TENER VALORES DE CERO. LOS CEROS ENMASCARAN LOS OTROS VALORES.
-# CUANDO SE SOBRELAPAN LOS RASTER, EL ULTIMO RASTER QUEDARA ENCIMA DE TODOS LOS DEMAS.
-# Es decir que si se dejan los ceros, estos se sobrelaparan con los rasters que 
-#quedan debajo y es como si eliminaran los valores de 1 de los otros rasters. 
+#3. Set biannual forest loss calculation ------
+def<-function(t1, t2){
+  floss<-(foct1 - foct2)
+  return(floss)
+  }
+#4 Run the function over the list 
+    ydef<-future(map(2:(length(tiffs1)-1), function(x) def(tiffs1[[x]],tiffs1[[x+1]])) 
+    
 
-m <- c(NA, NA, 0)
-m <- matrix(m, ncol=3, byrow=TRUE)
-H2018 <- reclassify(H2018,m)
-H2017 <- reclassify(H2017,m)
-
-
-#3. Calculate Forest loss between years ------
-D17 <- H2017 - H2018
-D16 <- H2016 - H2017
-plot(D16)
-
-#4. Reclasificar rasters en caso de que alguno tenga valores -1. ------- 
-#Este valor representa ganancia, pero hay que descartarlo porque lo que nos importa es 
-# lo que se perdio. 
-g <- c(-Inf, 0, NA)
-g <- matrix(g, ncol=3, byrow=TRUE)
-D16 <- reclassify(D16, g)
-
-#5. Guardar raster Deforestacion. Estos raster tendrán valores 0-1. En mi flujo
+#5. Guardar raster Deforestacion. Estos raster tendrÃ¡n valores 0-1. En mi flujo
 # de trabajo en el siguiente paso para hacer Merge, tuve que reclasificarlos 
 # por segunda vez en 1-NA, para remover los ceros. 
 writeRaster(D17, filename = "Defores2017_2018", format="GTiff", overwrite=TRUE)
@@ -102,8 +82,8 @@ D17<- reclassify(rasters[[17]], rcl)
 #Merge
 #Cuando se hace merge, el elemento 1 de la lista es el que queda encima de todo
 # Por eso empiezo a hacer merge
-# desde el ultimo año de perdida de bosque (2018-2017), para quede quede encima 
-#e  ir adicionando los siguientes años hacia atrás
+# desde el ultimo aÃ±o de perdida de bosque (2018-2017), para quede quede encima 
+#e  ir adicionando los siguientes aÃ±os hacia atrÃ¡s
 
 A1 <- merge(D18, D17)
 A2 <- merge(A1, D16)
