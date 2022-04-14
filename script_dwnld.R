49b6c9f2980706492580d49a955c37c6ad26525e
Jeronimo Rodriguez-Escobar <38987055+Cumaribo@users.noreply.github.com>
2022-04-14 18:31:40 -0400

    Update script_dwnld.R
    Just tested it. Make sure this is for a single (multi)polygon
    Can be mapped. Only limitation: memory

--- script_dwnld.R
+++ script_dwnld.R
@@ -1,39 +1,26 @@
-# Map downloader. I intend to develop some way to download the maps that i need using 
-# the thresahodls I extracted.
+# Hansen forest  Map Downloader Using Ecochange and determining threshold from an attribute table. 
 
-setwd("~/Documents/biomas_iavh/Matrices_Abril")
+setwd("~/Documents/biomas_iavh/Matrices_Abril") (#keep in mind for the documentation, remove after)
 
-library(raster)
-library(sf)
-library(forestChange)
-library(maptools)
-library(tidyverse)
 
 packs <- c('raster','rgdal','parallel', 'R.utils', 'rvest','xml2','tidyverse', 'landscapemetrics', 'sf','dplyr','httr','getPass','gdalUtils','gdalUtilities','rgeos', 'viridis',
            'rasterVis','rlang', 'rasterDT', 'ecochange')
-
 out.. <-  sapply(packs, require, character.only = TRUE)
-
 
 gdalUtils::gdal_setInstallation()
 valid_install <- !is.null(getOption("gdalUtils_gdalPath"))
-#load shapefiles
-
-
+#load Vector Datsa ROI
+           # It uses the attribute table to extract data labeling and parameter defiition information (name spatial unit) and splits in the different 
+           # objects 
 masked <- st_read('biomes_attributes_msk.shp')
 labels <- (masked$biome)
 masked <- as(masked, 'Spatial')
 # split mun into list of independent polygons  
 biomat <- masked%>%split(.$biome)
 
-test <- biomat[[1]]
-test <- as(test, 'Spatial')
-plot(test)
-
-
-thr <- test$thrshl
-
-
+           #Run individual example (documentation R)
+ test<- biomat[[i]]
+           
 suppressWarnings(
   def <- echanges(test,   # polígono 
                   lyrs = c('treecover2000','lossyear'),      # nombres de las capas
@@ -42,18 +29,3 @@ suppressWarnings(
                   change_vals = seq(0,18,1),      # en este caso, los años de pérdida, 
                   mc.cores = 9)     # número de núcleos, solo funciona en sistema linux
 )
-
-c(70,100)
-?echanges
-?seq
-suppressWarnings(
-  def <- echanges(pol,   # polígono 
-                  lyrs = c('treecover2000','lossyear'),      # nombres de las capas
-                  path = td,      # directorio de trabajo, en caso de que no desees trabajar en el directorio temporal
-                  eco_range = c(70, 100),      # Umbral de treecover2000
-                  change_vals = seq(0,15,5),      # en este caso, los años de pérdida, 
-                  mc.cores = 9)     # número de núcleos, solo funciona en sistema linux
-)
-
-
-
