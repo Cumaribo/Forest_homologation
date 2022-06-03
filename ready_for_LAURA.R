d5cee740795ceda20d91950cd52782afb33f78a0
Cumaribo <jeronimo.rodriguez@temple.edu>
2022-06-02 22:22:59 -0400

    New version. AddeSplitRas bexuse it crashed.

--- ready_for_LAURA.R
+++ ready_for_LAURA.R
@@ -1,54 +1,16 @@
 # This code reviewed on 18/04/2022
 
-getwd()
-
-rm(list=ls())
-
-
-install.packages('SDMTools','http://www.rforge.net/')
-install.packages('quantreg')
-
-install.packages('useful')
-install.packages('diffeR')
-install.packages('bcp')
-install.packages('bfast')
-install.packages('Kendall') 
-
-
-install.packages("greenbrown", repos="http://R-Forge.R-project.org")
 
 ##############################
-packs<-c('raster', 'rgdal', 'tidyverse', 'useful','diffeR', 'greenbrown', 'gdalUtils', 'gdalUtilities')
+packs<-c('raster', 'rgdal', 'tidyverse', 'useful','diffeR', 'greenbrown', 'gdalUtils', 'gdalUtilities', 'furrr')
 sapply(packs, require, character.only = TRUE) 
 
-
-library(raster)
-#library(forestChange)
-#library(parallel)
-#library(sf)
-library(tidyverse)
-#library(purrr)
-library(furrr)
-library(diffeR)
-library(useful)
-library(greenbrown)
-library(data.table)
-
-
-dir.create('tempfiledir')
+#Ready environment
+#dir.create('tempfiledir')
 tempdir=paste(getwd(),'tempfiledir', sep="/")
 rasterOptions(tmpdir=tempdir)
-# unixtools::set.tempdir('/media/mnt/Ecosistemas_Colombia/tempfiledir')
 
-
-# this function uses the package diffeR to compare between pairs of maps. It does not produce agreement maps, only contingency matrices. It works faster and does not require that both maps have pixels form all classes
-#######################THIS IS THE FUNCTION THAT I NEED TO USE NOW (03/03/2021) Here we are, on 18/04/2022
-# still messing with this thing, but i finally know where we're going. 
-
-
-
-#1. Load the agreement maps:
-  
+#1. Reproject/align
 hansen19<-raster('/media/mnt/Forest_Armonization/Armonized_nomsk/armonized_2019a.tif')
 
 target<-'/media/mnt/Forest_Armonization/SMBYC_Data/cambio_2018_2019_v8_200707_3116.img'
@@ -56,47 +18,67 @@ reference. <-"/media/mnt/Forest_Armonization/mask_ideam90_17.tif"
 dst.<-'/media/mnt/Forest_Armonization/cambio18_19.tif'
 align_rasters(unaligned=target, reference=reference., dstfile=dst., nthreads=6, verbose= TRUE)
 
-
+#Homologue Classes
 ideam19<-raster('/media/mnt/Forest_Armonization/cambio18_19.tif')
-
 m<-c(1.9, 2.1, 5, 3.9, 4.1, 1)
 m<-matrix(m, ncol=3, byrow=TRUE)
 ##### r
 ideam19<-reclassify(ideam19, m)                                        # imagen a alienar
- 
-writeRaster(ideam19, 'ideamfnF2019.tif')
-
-
+ writeRaster(ideam19, 'ideamfnF2019.tif')
 msk<-raster('/media/mnt2/BiomapCol_22/mask_colombia.tif')
-
 m<-c(4.9, 5.1, 0, 0,0, NA)
-
 m<-matrix(m, ncol=3, byrow=TRUE)
 ideam19<-reclassify(ideam19, m)                                     
 
-setwd('/media/mnt/Forest_Armonization/')
-
-
 ideam19<-mask(ideam19, msk)
 writeRaster(ideam19,'ideamfnF2019_rec.tif', overwrite=TRUE)
-
 m<-c(NA, NA, 0)
 m<-matrix(m, ncol=3, byrow=TRUE)
 
 hansen19<-reclassify(hansen19, m)
 hansen19<-mask(hansen19, msk)
-writeRaster(hansen19,'hansen2019_bin.tif', overwrite=TRUE)
+writeRaster(hansen19, 'hansen2019_bin.tif', overwrite=TRUE)
+
+#load hansen ideam
+ideam19<-raster('ideamfnF2019_rec.tif')
+hansen19<-raster('hansen2019_bin.tif')
+
+SplitRas <- function(raster,ppside,save,plot){
+    h        <- ceiling(ncol(raster)/ppside)
+    v        <- ceiling(nrow(raster)/ppside)
+    agg      <- aggregate(raster,fact=c(h,v))
+    agg[]    <- 1:ncell(agg)
+    agg_poly <- rasterToPolygons(agg)
+    names(agg_poly) <- "polis"
+    r_list <- list()
+    for(i in 1:ncell(agg)){
+        e1          <- extent(agg_poly[agg_poly$polis==i,])
+        r_list[[i]] <- crop(raster,e1)
+    }
+    if(save==T){
+        for(i in 1:length(r_list)){
+            writeRaster(r_list[[i]],filename=paste("SplitRas",i,sep=""),
+                        format="GTiff",datatype="FLT4S",overwrite=TRUE)
+        }
+    }
+    if(plot==T){
+        par(mfrow=c(ppside,ppside))
+        for(i in 1:length(r_list)){
+            plot(r <- list[[i]],axes=F,legend=F,bty="n",box=FALSE)
+        }
+    }
+    return(r_list)
+        }
 
 
-agg<-hansen19*ideam19
+ideam19sp<-SplitRas(ideam19, ppside=12, save=FALSE, plot=FALSE)
+hansen19sp<-SplitRas(hansen19, ppside=12, save=FALSE, plot=FALSE)
+rm(ideam19, hansen19)
+mem_future <- 5000*1024^2 #this is toset the limit to 1GB
+plan(multisession, workers=5)
+options(future.globals.maxSize= mem_future)
+comparedata<- future_map(1:length(ideam19sp), function(x) CompareClassification(ideam19[[x]], hansen19[[x]], names = list('Ideam_19'=c('no-Forest','forest', 'no-data'),'Hansen_19'=c('no-Forest','forest','no-data')), samplefrac = 1)
+                        agg<-do.call(merge, comparedata$raster)
+                         writeRaster(agg, 'agg_for_19.tif')
 
-writeRaster(agg, 'aggForest_2019.tif')
-
-comparer2 <- function(ideam1,ideam2, perc){
-  test1 <- crosstabm(ideam1, ideam2, percent=perc, population=NULL)
-  return(test1)}
-
-comparedata<- CompareClassification(ideam19, hansen19, names = list('Ideam_19'=c('no-Forest','forest', 'no-data'),
-                                                                    'Hansen_19'=c('no-Forest','forest','no-data')), samplefrac = 1)
-writeRaster(comparedata$raster, 'agg_for_19.tif')
-write.csv(comparedata$table, file='cont_mat.csv')
+                        write.csv(comparedata$table, file='cont_mat.csv')
