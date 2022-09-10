d4e034556af98db50e73b65fb1a1886af2b53a9a
Cumaribo <tug76452@github.com>
2022-09-09 20:30:22 -0400

    Retaking the work with National Parks. For theiur documentation. Just closed the script in R and will keep the path to the location

--- R/test_PNN.R
+++ R/test_PNN.R
@@ -5,103 +5,41 @@ out.. <-  sapply(packs, require, character.only = TRUE)
 setwd('/storage/home/TU/tug76452/Forest_Armonization')
 
 
-
 dir.create('tempfiledir')
                                         #obtain string with the path
 tempdir=paste(getwd(),'tempfiledir', sep="/")
 rasterOptions(tmpdir=tempdir)
 
-
-td <- getwd()
-#Load vector data as SF
-# 1 . Set files
-
-shps <- list.files('/storage/home/TU/tug76452/Forest_Armonization/SINAP_areas/', pattern='shp')
-shps <- map(1:length(shps), function(x) str_sub(shps[x], start=1, end=9))
-shps <- unlist(shps)
-setwd('/storage/home/TU/tug76452/Forest_Armonization/SINAP_areas/')
-# st_read
-sfs <- map(1:6, function(x) st_read('.', shps[x]))
-
-#3 Load template to rasterize
-temp <- raster('/storage/share/BiomapCol_22/mask_colombia.tif')
-
-#temp2 <- raster('/Users/sputnik/Documents/bosque-nobosque/IDEAMfnf/msk_SMBYC.tif')
-#4. Reproject to the crs of the template
-sfs <- map(1:6, function(x) st_transform(sfs[[x]], crs(temp)))
-#5 Add numeric attribute to rasterize
-sfs <- map(1:length(sfs), function(x) mutate(sfs[[x]], type = case_when(NIVEL2 == 'Aguas Continentales' ~ 1,
-                                          NIVEL2 == 'Aguas Maritimas' ~ 2, 
-                                          NIVEL2 == 'Areas Abiertas, sin o con poca Vegetacion' ~3,
-                                          NIVEL2 == 'Areas Agricolas Heterogeneas' ~ 4,
-                                          NIVEL2 == 'Areas con Vegetacion Herbacea y/o Arbustiva' ~ 5,
-                                          NIVEL2 == 'Areas Humedas Continentales' ~ 6,
-                                          NIVEL2 == 'Areas Humedas Costeras' ~ 7,
-                                          NIVEL2 == 'Bosques' ~8,
-                                          NIVEL2 == 'Cultivos Permanentes' ~9,
-                                          NIVEL2 == 'Nubes' ~ 10,
-                                          NIVEL2 == 'Pastos'~ 11,
-                                          NIVEL2 == 'Zonas Industriales o Comerciales y Redes de Comunicacion' ~ 12,
-                                          NIVEL2 == 'Zonas Verdes Artificializadas, no Agricolas' ~ 13)))
-#Fix name of sfs[[6]]
-sfs[[6]] <- sfs[[6]]%>%mutate(NOMBRE = NOM)
-#test to nest. However this is a list not a tidy object, andbtw, the number and names of the attributes is differnet, as 
-                                        #well as the order. This would require some extra work. Let us keep it like this
-# Next to fix: learn how to nest the lists
- #sfs_nest <- sfs%>%group_by(.$NOMBRE)%>%nest()
-
-sfs. <- unlist(sfs)
-
-#Split by PNN
-sfs1 <- sfs[[1]]%>%split(.$NOMBRE)
-sfs2 <- sfs[[2]]%>%split(.$NOMBRE)
-sfs3 <- sfs[[3]]%>%split(.$NOMBRE)
-sfs4 <- sfs[[4]]%>%split(.$NOMBRE)
-sfs5 <- sfs[[5]]%>%split(.$NOMBRE)
-sfs6 <- sfs[[6]]%>%split(.$NOMBRE)
-
-#Map inside a map
-sfs. <- map(sfs, . %>% split(.$NOMBRE))
-
-
-sfs.[[1]][2]
-
-sfs1[[1]]
-
-
-#Extract Vector with the names of the PNN (surely will need them in a moment)
-names <- unique(sfs[[1]][["NOMBRE"]])
-names <- sort(names)
-#load harmonized masked 
+#  Load 0 background raster   to rasterize
+temp <- raster('/storage/home/TU/tug76452/Forest_Armonization/bin_masked/msk_0.tif')#load harmonized masked 
 mskwd <- '/storage/share/BiomapCol_22/Maps_hansen/Armonized_msk'
 
 tiffes <- list.files(mskwd, pattern='.tif')
 #select the specific years
 
-tiffes <- tiffes[-c(1:5)]
+tiffes <- tiffes[-c(1:6)]
 tiffes <- tiffes[c(3,8,13,16,18,19)]
                                         # set vector to name rasters per year.
 years <- unlist(map(1:length(tiffes), function(x) str_sub(tiffes[x], start=8, end=15)))
 
-                                        #load rasters masks 
-getwd()
 
 harm <-list()
 for(i in 1:length(tiffes)){
   harm[i] <- raster(paste(mskwd,tiffes[i], sep='/'))}
 #load mask (fill NAs, i think i have it somewhere but can't recall where)
-
-m <- c(-Inf, Inf, 0)
-m <- matrix(m,ncol=3,byrow=TRUE)
-temp <- reclassify(temp,m)
-
 # crop the template raster
 # for some reason future_map s not working on the Mac. Find out why
- mem_future <- 5000*1024^2 #this is toset the limit to 5GB
+
+mem_future <- 5000*1024^2 #this is toset the limit to 5GB
  plan(multisession, workers=6)
  options(future.globals.maxSize= mem <- future)
 
 harm <- map(1:length(harm), function(x) raster::merge(harm[[x]], temp))
+binwd <- '/storage/home/TU/tug76452/Forest_Armonization'
+setwd(binwd)
+map(1:length(harm), function(x) writeRaster(harm[[x]], paste('bin_masked', years[x], sep= '_')))
+
+dir()
 
 #crop and mask for each of the PNN
 temp <-map(1:length(sfs1), function(x)  crop(temp, extent(sfs1[[x]]))) 
