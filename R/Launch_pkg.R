d4c5ec4be6a7070738f9d205479743fbd69fb95a
Cumaribo <tug76452@github.com>
2023-02-21 13:30:25 -0500

    Try to donwload the data in Laptop, Nimbus is out of order

--- R/Launch_pkg.R
+++ R/Launch_pkg.R
@@ -2,9 +2,11 @@
 # I need to cnvert this into a Rproj file
 #Load greenbrown from source
 #load libraries.
-packs <- c('raster','rgdal', 'tidyverse', 'fasterize', 'landscapemetrics', 'sf',
-           'diffeR', 'greenbrown', 'gdalUtils','gdalUtilities', 'rgeos', 'rasterDT', 
+
+packs <- c('raster', 'tidyverse', 'fasterize', 'landscapemetrics', 'sf',
+           'diffeR', 'rgeos', 'rasterDT', 
            'ecochange', 'furrr')
+
 out.. <-  sapply(packs, install.packages, character.only = TRUE)
 out.. <-  sapply(packs, require, character.only = TRUE)
 
@@ -16,4 +18,7 @@ rasterOptions(tmpdir=tempdir)
 # Today, 25/07/2022 future_map is not working on Mac or Linux. Find out why. 
 mem_future <- 5000*1024^2 #this is to set the limit to 5GB
 plan(multisession, workers=7)
-options(future.globals.maxSize= mem <- future)
+options(future.globals.maxSize= mem_future)
+
+
+packs
