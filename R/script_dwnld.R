0345b13ea03ac4e76841d3e5fe02e4137c8b227b
Jeronimo Rodriguez <jeronimo.rodriguez@temple.edu>
2023-02-21 17:57:12 -0500

    Fixed some typos in the echanges()map. Was trying it in nimbus and of course it was never going to finish like this because I am a moron. Try again in Nimbus

--- R/script_dwnld.R
+++ R/script_dwnld.R
@@ -2,19 +2,15 @@
                                         # Hansen forest  Map Downloader Using Ecochange and determining threshold from an attribute table. 
 
 setwd("~/Documents/biomas_iavh/Matrices_Abril") #keep in mind for the documentation, remove after)
-
-
-packs <- c('terra', 'raster','rgdal','parallel', 'R.utils', 'rvest','xml2','tidyverse', 'landscapemetrics', 'sf','dplyr','httr','getPass','gdalUtilities','rgeos',
+setwd('/Users/sputnik/Documents/biomas_iavh/Biomas IAvH')
+packs <- c('terra', 'raster','parallel', 'R.utils', 'rvest','xml2','tidyverse', 'landscapemetrics', 'sf','dplyr','httr','getPass',
            'rasterVis','rlang', 'rasterDT', 'ecochange')
 
 #out.. <-  sapply(packs, install.packages, character.only = TRUE)
 out.. <-  sapply(packs, require, character.only = TRUE)
 
-
-
-gdalUtils::gdal_setInstallation()
-valid_install <- !is.null(getOption("gdalUtils_gdalPath"))
-
+#gdalUtils::gdal_setInstallation()
+#valid_install <- !is.null(getOption("gdalUtils_gdalPath"))
 setwd('~/Forest_Armonization/FINAL_HANSEN/output_armonization')
 
 getwd()
@@ -23,17 +19,17 @@ getwd()
            # It uses the attribute table to extract data labeling and parameter definition information (name spatial unit) and splits in the different 
            # objects 
 masked <- st_read('biomes_attributes_msk.shp')
-labels <- (masked$biome)
+masked <- st_read('/Users/sputnik/Documents/biomas_iavh/Final_results_Codename_Abril/biomes_attributes_msk.shp')
+
 
                                         #here, you need to filter and select the biome you need. You can filter sf objects with tidyverse)
 masked <- masked%>%subset(!is.na(accurcy))
+labels <- (masked$biome)
 masked <- as(masked, 'Spatial')
 # split mun into list of independent polygons. You don't need it 
 #biomat <- masked%>%split(.$biome)
 
 biomat <- masked%>%split(.$biome)
-
-
            #Run individual example (documentation R)
 #with one polygon           
 suppressWarnings(
@@ -47,13 +43,13 @@ suppressWarnings(
 
 #with a list of polygons
 suppressWarnings(
-  def <- map(1:length(masked, function(x) echanges(biomat[[x]],   # polígono 
+  def <- map(1:length(masked), function(x) echanges(biomat[[x]],   # polígono 
                   lyrs = c('treecover2000','lossyear'),      # nombres de las capas
                   path = getwd(),      # directorio de trabajo, en caso de que no desees trabajar en el directorio temporal
                   eco_range = c(biomat[[x]]$thrshld,100),      # Umbral de treecover2000
                   change_vals = seq(0,21,1),      # en este caso, los años de pérdida, 
                   mc.cores = 7)     # número de núcleos, solo funciona en sistema linux
 ))
-
+)
 
 biomat[[1]]$thrshld
